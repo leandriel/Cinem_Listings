f06b76e2b509dbbb2209fb6ab658831ed2f69ad3
Leandro Valderas <ARX0001451@prosegur.com>
2023-01-31 13:14:46 -0300

    Creacion de Home y Details

---
+++ app/src/main/java/com/leandroid/apps/cinemalistings/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.leandroid.apps.cinemalistings.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leandroid.apps.cinemalistings.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
